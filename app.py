26b994e826a224c3dfc4b74ff497d64b81bc3ee1
NASEEMABANU B <naseemagithub@gmail.com>
2025-11-29 17:06:01 +0530

    Remove unused seaborn import
    Removed seaborn import from app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import plotly.express as px
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
